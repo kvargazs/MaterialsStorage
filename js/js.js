19c7ad1154104db901013d8b15b7e825e4cea384
kvargazs <karol.v.0570@gmail.com>
2025-05-08 15:58:41 -0300

    atualizacao da home

--- js/js.js
+++ js/js.js
@@ -11,3 +11,24 @@ links.forEach(function(link) {
         link.classList.add('active');
     }
 });
+
+
+
+// adicionar um item dinamicamente
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.getElementById("itens-container");
+
+    // Função para adicionar um novo item
+    function adicionarItem(nomeItem) {
+        const novoItem = document.createElement("div");
+        novoItem.classList.add("itens");
+        novoItem.innerHTML = nomeItem;  // O conteúdo do item pode ser o nome do material ou qualquer coisa
+        container.appendChild(novoItem);
+    }
+
+    // Exemplo de adicionar 10 itens
+    for (let i = 1; i <= 8; i++) {
+        adicionarItem(`ITEM ${i}`);
+    }
+});
+
