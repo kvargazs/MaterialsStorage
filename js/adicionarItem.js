73daea7baf63154c7157db2ce08c87983876aa6b
Fellipe <fellipe.scheffer@aluno.senai.br>
2025-06-06 15:41:31 -0300

    resolvendo

--- js/adicionarItem.js
+++ js/adicionarItem.js
@@ -3,29 +3,27 @@ function EnviarItem() {
     const codigo = document.getElementById('input_codigo').value;
     const complemento = document.getElementById('input_complemento').value;
     const quantidade = document.getElementById('input_quantidade').value;
-<<<<<<< HEAD
     
     // Segmento permite múltiplas opções (via Select2)
-    const segmento = $('input_segmento').val();  // array
+    const segmento = $('#input_segmento').val();  // array
 
     // Unidade é seleção simples
-=======
-    const segmento = $('#input_segmento').val();
->>>>>>> b77ee1ae65459ada326301d65ae1c7575ff8fa51
     const unidade = document.getElementById('input_unidade').value;
 
-    if (!nome || !codigo || !segmento || !complemento || !unidade || !quantidade) {
+    // Validação
+    if (!nome || !codigo || !segmento.length || !complemento || !unidade || !quantidade) {
         alert("Todos os campos precisam ser preenchidos!");
         return;
     }
 
+    // Envia para o backend
     fetch('http://localhost:5000/inserir', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             nome,
             codigo,
-            segmento,
+            segmento,   // array
             complemento,
             unidade,
             quantidade
@@ -40,3 +38,6 @@ function EnviarItem() {
         alert('Erro ao enviar o item.');
     });
 }
+
+
+
